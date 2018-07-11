e4c4f01e124f8ed343c407086bd4ecd3634ba0b6
sanye1002 <331139839@qq.com>
2018-07-11 11:17:54 +0800

    修改

--- src/main/java/cn/popo/news/NewsApplication.java
+++ src/main/java/cn/popo/news/NewsApplication.java
@@ -1,7 +1,13 @@
 package cn.popo.news;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -33,4 +39,33 @@ public class NewsApplication {
 		};
 
 	}
+	/**
+	 * it's for set http url auto change to https
+	 */
+/*	@Bean
+	public EmbeddedServletContainerFactory servletContainer(){
+		TomcatEmbeddedServletContainerFactory tomcat=new TomcatEmbeddedServletContainerFactory(){
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint=new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");//confidential
+				SecurityCollection collection=new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(httpConnector());
+		return tomcat;
+	}
+
+	@Bean
+	public Connector httpConnector(){
+		Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8888);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}*/
 }
